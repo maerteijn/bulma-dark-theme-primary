1d5d565cb051b0dec57f9b1d16be4b7465fa99d9
Csaba Kiss <csaba.kiss@parsons.us>
2025-01-14 12:48:36 -0700

    fixed themem toggling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './scss/main.scss';
 
 document.querySelector("#btn-toggle-theme").addEventListener("click", (e) => {
-    document.documentElement.classList.toggle("theme-dark");
+    document.documentElement.dataset.theme = document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
 });
